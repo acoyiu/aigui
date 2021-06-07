1cc5a9081b19eede2d821ef35d907334c0fe7462
Aco Kong <acokong.ppwi@gmail.com>
2021-06-07 15:34:37 +0800

    try redirect

--- _lib/InitSvgText.js
+++ _lib/InitSvgText.js
@@ -1,41 +1,51 @@
 async function InitSvgText(tagToUpdateInnerHTML) {
 
-    const
-        urlParams = new URLSearchParams(window.location.search),
-        pathParam = decodeURI(urlParams.get('path'));
-    // console.log('pathParam', pathParam);
-
-    
-    window.targetPath = pathParam;
+    try {
+        const
+            urlParams = new URLSearchParams(window.location.search),
+            pathParam = decodeURI(urlParams.get('path'));
+        // console.log('pathParam', pathParam);
 
 
-    let svgText = await (await fetch(`${pathParam}/index.svg`)).text();
+        if (!pathParam)
+            throw 'No param passed in.';
 
 
-    svgText = svgText
-        .replace(/_x21_/g, '!')
-        .replace(/_x40_/g, '@')
-        .replace(/_x23_/g, '#')
-        .replace(/_x24_/g, '$')
-        .replace(/_x25_/g, '%')
-        .replace(/_x5E_/g, '^')
-        .replace(/_x26_/g, '&')
-        .replace(/_x2A_/g, '*')
-        .replace(/_x28_/g, '(')
-        .replace(/_x29_/g, ')')
-        .replace(/_x2D_/g, '-')
-        .replace(/_x5F_/g, '_')
-        .replace(/_x2B_/g, '+')
-        .replace(/_x3D_/g, '=')
-        .replace(/_x2C_/g, ',')
-        .replace(/<\?xml(.*?)>/g, '')
-        .replace(/<!--(.*?)-->/g, '')
-        .replace(
-            /xlink\:href\=\"/g,
-            `xlink:href="${pathParam}/`
-        );
-    // console.log(svgText);
+        window.targetPath = pathParam;
 
 
-    tagToUpdateInnerHTML.innerHTML = svgText;
+        let svgText = await (await fetch(`${pathParam}/index.svg`)).text();
+
+
+        svgText = svgText
+            .replace(/_x21_/g, '!')
+            .replace(/_x40_/g, '@')
+            .replace(/_x23_/g, '#')
+            .replace(/_x24_/g, '$')
+            .replace(/_x25_/g, '%')
+            .replace(/_x5E_/g, '^')
+            .replace(/_x26_/g, '&')
+            .replace(/_x2A_/g, '*')
+            .replace(/_x28_/g, '(')
+            .replace(/_x29_/g, ')')
+            .replace(/_x2D_/g, '-')
+            .replace(/_x5F_/g, '_')
+            .replace(/_x2B_/g, '+')
+            .replace(/_x3D_/g, '=')
+            .replace(/_x2C_/g, ',')
+            .replace(/<\?xml(.*?)>/g, '')
+            .replace(/<!--(.*?)-->/g, '')
+            .replace(
+                /xlink\:href\=\"/g,
+                `xlink:href="${pathParam}/`
+            );
+        // console.log(svgText);
+
+
+        tagToUpdateInnerHTML.innerHTML = svgText;
+    }
+    catch (error) {
+        alert('Content Load Error.');
+        throw error;
+    }
 };
--- readme.md
+++ readme.md
@@ -1,54 +1,5 @@
 1: Clone project
 git clone this project
 
-2: Start asset server
-npx http-server -c-1 --cors -p 7071 ./svg & npx http-server -c-1 --cors -p 7070 ./
-
-3: Build by Parcel
-parcel index.html
-
-
-
-
-
-Marker:
-
-Page@(Number)
-::PageNext()
-::PagePrev()
-::NavHideAt( number )
-::NavShowAt( number )
-::NavAtRange( number, number )
-
-
-pageConfiguration
-pageConfiguration::transitionMode::topDownScroll
-pageConfiguration::transitionMode::fade
-pageConfiguration::transition::ease*0.4
-
-
-Interactive 
-in::( name )
-::AddClickHide( name, toggle? )
-::AddClickShow( name, toggle? )
-::AddClickMove( name, x, y, r, toggle? )
-::PlayOnShow()
-::PlayOnMove( x, y, r )
-
-
-Transition Setup
-::transition=linear*0.5
-
-
-Audio
-audio::bgm::path
-audio::basic::path
-audio::name::path 
-
-
-GameSet()
-- RadiationDrag
-- ConnectLine
-- DragSlot 
-- Mc
-- Comparing
+2: Start Server
+http-server -c-1 --cors -p 7071 ./svg & http-server -c-1 --cors -p 7070
