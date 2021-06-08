f451be117344ea0e98efc1763968fffd14faa477
Aco Kong <acokong.ppwi@gmail.com>
2021-06-08 20:08:39 +0800

    add vertical support

--- _lib/InitSvgView.js
+++ _lib/InitSvgView.js
@@ -1,10 +1,15 @@
 async function InitSvgView() {
+
+    let isHorizontal = true;
+    if (window.location.search.includes('vertical=true'))
+        isHorizontal = false;
+
     AiGUI.SvgRootTag = document.querySelector('svg');
     AiGUI.TheView = new AiJs.View(
         AiGUI.SvgRootTag,
         {
             isMobile: true,
-            isHorizontal: true,
+            isHorizontal,
         }
     );
 }
